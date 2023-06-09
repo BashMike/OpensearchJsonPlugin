bf3f465106bc80a295c3fe028eda711ed2c81817
bashmike <greg.yakimov@gmail.com>
2023-06-09 14:42:32 +0300

    add :: console arguments count check.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 2) {
+            System.out.println("ERROR :: Invalid count of console arguments! Expected: " + 2 + ", got: " + args.length);
+            System.out.println("INFO  :: Usage of console program: java -jar json_parse <function name> testData.json");
+            System.out.println("        <function name> must be one of the following values: avg, max, values.");
+            System.exit(-1);
+        }
+
+        System.out.println("FUNCTION NAME: " + args[0] + ", JSON FILE NAME: " + args[1]);
     }
 }
